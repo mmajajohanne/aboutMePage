8912bc337a45f73a2bcc620db2932ce757473868
Maja Johanne <maja.marjamaa@gmail.com>
2022-09-29 12:21:43 +0200

    c

--- Jindex.js
+++ Jindex.js
@@ -1,5 +1,8 @@
 /*--scrollfunctions ---------*/
-function scrollToForm() {
+function scrollToAbout() {
     document.querySelector('#about').scrollIntoView({behavior: 'smooth'});
   }
+function scrollToProjects() {
+    document.querySelector('#projects').scrollIntoView({behavior: 'smooth'});
+}
 
