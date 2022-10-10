b200b751103a469e13085b0560cae581c9f4f32a
Maja Johanne <maja.marjamaa@gmail.com>
2022-10-10 22:57:11 +0200

    data tester

--- blog/script.js
+++ blog/script.js
@@ -16,9 +16,8 @@ let liEl3 = document.getElementById("idLi3")
 
 
 //for m integrering av json file 
+/*
 let data22 = $.csv.toObjects(csv)
-let
-        /*
         type: 'lines', 
         data: {
             datasets: [{
@@ -30,3 +29,31 @@ let
             xAxisKey: 'data\\.key',
             yAxisKey: 'data\\.value'
             }*/
+
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+         url: "data2.txt",
+         dataType: "text",
+         success: function(data) {processData(data);}
+    });
+});
+            
+function processData(allText) {
+     var allTextLines = allText.split(/\r\n|\n/);
+     var headers = allTextLines[0].split(',');
+     var lines = [];
+            
+     for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+         if (data.length == headers.length) {
+            
+            var tarr = [];
+             for (var j=0; j<headers.length; j++) {
+                 tarr.push(headers[j]+":"+data[j]);
+             }
+             lines.push(tarr);
+        }
+     }
+                // alert(lines);
+}
