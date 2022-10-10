aef07730f5c2d159e8b76fcaf718997e8bfb2699
Maja Johanne <maja.marjamaa@gmail.com>
2022-10-10 22:08:22 +0200

    c

--- blog/script.js
+++ blog/script.js
@@ -1,9 +1,9 @@
 let liEl1 = document.getElementById("idLi1")
-        let liEl2 = document.getElementById("idLi2")
-        let liEl3 = document.getElementById("idLi3")
+let liEl2 = document.getElementById("idLi2")
+let liEl3 = document.getElementById("idLi3")
 
 
-        //for m integrering av json file
+        //for m integrering av json file 
         liEl1.addEventListener("click", menufunc)
         liEl2.addEventListener("click", menufunc)
         liEl3.addEventListener("click", menufunc)
@@ -14,3 +14,17 @@ let liEl1 = document.getElementById("idLi1")
             liEl3.className = "";
             this.className = "active";
         }
+
+
+        /*
+        type: 'line',
+        data: {
+            datasets: [{
+                data: [{ 'data.key': 'one', 'data.value': 20 }, { 'data.key': 'two', 'data.value': 30 }]
+            }]
+        },
+        options: {
+            parsing: {
+            xAxisKey: 'data\\.key',
+            yAxisKey: 'data\\.value'
+            }*/
