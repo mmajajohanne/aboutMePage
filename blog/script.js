6d952e952400b24bf0442a79fa0239d0791b00c8
Maja Johanne <maja.marjamaa@gmail.com>
2022-10-10 23:01:14 +0200

    c

--- blog/script.js
+++ blog/script.js
@@ -1,19 +1,3 @@
-let liEl1 = document.getElementById("idLi1")
-let liEl2 = document.getElementById("idLi2")
-let liEl3 = document.getElementById("idLi3")
-
-        liEl1.addEventListener("click", menufunc)
-        liEl2.addEventListener("click", menufunc)
-        liEl3.addEventListener("click", menufunc)
-
-        function menufunc(){
-            liEl1.className = "";
-            liEl2.className = "";
-            liEl3.className = "";
-            this.className = "active";
-        }
-
-
 
 //for m integrering av json file 
 /*
