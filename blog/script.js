9827087656a684d48d402a193c6b440cf7db226a
Maja Johanne <maja.marjamaa@gmail.com>
2022-10-11 14:59:49 +0200

    c

--- blog/script.js
+++ blog/script.js
@@ -14,36 +14,32 @@ let liEl1 = document.getElementById("idLi1")
             //hide and replace "home", "archive" and "about" div
             }
 
-        //DATA
-        let mainDataArr = new Array()
-
-        d3.csv("data2.csv", function(data) {
-            for (var i = 0; i < data.length; i++) {
-                console.log(data[i].Title);
-                console.log(data[i].Date);
-                console.log(data[i].Text);
-            }
-            for (var i = 0; i < data.length; i++) {
-                mainDataArr.push(data[i].Title);
-                mainDataArr.push(data[i].Date);
-                mainDataArr.push(data[i].Text);
-            }
-
-            const perChunk = 3 // elements per chunk ("Title", "Date", "Txt")
-            const result = mainDataArr.reduce((resultArray, item, index) => { 
-            const chunkIndex = Math.floor(index/perChunk)
-
-            if(!resultArray[chunkIndex]) {
-                resultArray[chunkIndex] = [] // start a new chunk
-            }
-            resultArray[chunkIndex].push(item)
-            return resultArray
-            }, [])
-
-            console.log(result); 
-            console.log(result[1][1]);
+//include
+let dato1El = document.getElementById("idDato1")
             
-            //sortere arrayen basert på dato? altså element 1 i hver av arrayene
-            
-        });
+//DATA
+let mainDataArr = new Array()
+
+d3.csv("data2.csv", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        mainDataArr.push(data[i].Title);
+        mainDataArr.push(data[i].Date);
+        mainDataArr.push(data[i].Text);
+    }
+
+    const perChunk = 3; // elements per chunk ("Title", "Date", "Txt")
+    const result = mainDataArr.reduce((resultArray, item, index) => { 
+        const chunkIndex = Math.floor(index/perChunk);
+
+        if(!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = [] // start a new chunk
+        };
+        resultArray[chunkIndex].push(item);
+        return resultArray;
+    }, []);
+
+    //console.log(result); 
+    //console.log(result[1][1]);
+    dato1El.innerHTML = result[0][1]
+});
         
