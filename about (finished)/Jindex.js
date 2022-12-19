d57ad627fe0b81ab609b618db17eb9fc9c55cf49
Maja Marjamaa <82090456+mmajajohanne@users.noreply.github.com>
2022-12-19 12:47:46 +0100

    Delete about (finished) directory

--- about (finished)/Jindex.js
+++
@@ -1,8 +0,0 @@
-/*--scrollfunctions ---------*/
-function scrollToAbout() {
-    document.querySelector('#about').scrollIntoView({behavior: 'smooth'});
-  }
-function scrollToProjects() {
-    document.querySelector('#projects').scrollIntoView({behavior: 'smooth'});
-}
-
